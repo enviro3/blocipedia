65a8870552b8203e7fb8386aa0676fc1158ad336
Caroline Vurlumis <cvurlumis@gmail.com>
2017-11-06 20:43:30 -0800

    user-story-6, faker gem added users and wikis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106012858) do
+ActiveRecord::Schema.define(version: 20171107014931) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171106012858) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "role"
+    t.string   "name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,39 +8,45 @@
 
 require 'faker'
 
-#create a fake User
+# create fake users
 5.times do
-  user = User.new(
+  user = User.create!(
     name: Faker::Name.name,
     email: Faker::Internet.email,
     password: "password"
   )
 end
 
-# puts "#{User.count} users were created!"
-#
-# #create a fake wiki
-# wiki = Faker::Wiki.name
-#
-#
-# # Create an admin user
-#  admin = User.create!(
-#    name:     'Admin User',
-#    email:    'admin@example.com',
-#    password: 'helloworld',
-#    role:     'admin'
-#  )
-#
-#  # Create a member
-#  member = User.create!(
-#    name:     'Member User',
-#    email:    'member@example.com',
-#    password: 'helloworld'
-#  )
-#
-#  # Create a premium member
-#  premium = User.create!(
-#    name:     'Premium User',
-#    email:    'premium@example.com',
-#    password: 'helloworld'
-#  )
+puts "#{User.count} users were created!"
+
+# create fake wikis
+30.times do
+  wiki = Wiki.create!(
+    title: Faker::Job.title,
+    body: Faker::Hobbit.quote
+  )
+end
+
+puts "#{Wiki.count} wikis were created!"
+
+# create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+# create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+
+# create a premium member
+ premium = User.create!(
+   name:     'Premium User',
+   email:    'premium@example.com',
+   password: 'helloworld'
+ )
